57062a65ca217770007fc69b6a695581dba6339c
Xuan Cuong <94371015+xuancuong244@users.noreply.github.com>
2023-10-12 11:06:02 +0700

    CHiNhanhRestController

--- Cinema/src/main/java/com/cinema/RestController/ChiNhanhRestController.java
+++ Cinema/src/main/java/com/cinema/RestController/ChiNhanhRestController.java
@@ -1,4 +1,29 @@
 package com.cinema.RestController;
 
+import com.cinema.Entity.ChiNhanh;
+import com.cinema.Services.ChiNhanhService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@CrossOrigin("*")
+@RestController
+@RequestMapping("/api/ChiNhanh")
 public class ChiNhanhRestController {
+    @Autowired
+    ChiNhanhService chiNhanhService;
+    // http method: GET, POST, PUT, DELETE
+
+    // localhost:8085/api/ChiNhanh/all
+    @GetMapping()
+    public ResponseEntity<?> getAll(){
+        List<ChiNhanh> chiNhanh = chiNhanhService.findAll();
+
+        return ResponseEntity.ok(chiNhanh);
+    }
 }
