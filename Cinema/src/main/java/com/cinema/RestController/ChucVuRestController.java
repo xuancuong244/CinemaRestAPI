5da296165a6ef80729a72f6a73e9678d117f1edc
Xuan Cuong <94371015+xuancuong244@users.noreply.github.com>
2023-10-23 09:56:20 +0700

    update

--- Cinema/src/main/java/com/cinema/RestController/ChucVuRestController.java
+++ Cinema/src/main/java/com/cinema/RestController/ChucVuRestController.java
@@ -16,7 +16,7 @@ public class ChucVuRestController {
     ChucVuService chucVuService;
 
     // localhost:8085/api/chucVu/all
-    @GetMapping()
+    @GetMapping("/all")
     public ResponseEntity<?> doGetAll(){
         List<ChucVu> chucVus = chucVuService.findAll();
         return ResponseEntity.ok(chucVus);
